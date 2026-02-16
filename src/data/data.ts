f0d4c5b9c955c3a68a8247203d6dfd5fc24f6d26
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2026-02-16 12:38:44 -0800

    ""

---
+++ src/data/data.ts
@@ -0,0 +1,65 @@
+export const test = [
+    {
+        id: 1,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 2,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 3,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 4,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 5,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 6,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 7,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 8,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+    {
+        id: 9,
+        title: "CPU nima?",
+        body: "CPU - Central Process Unit Markaziy protsessor yoki markaziy protsessor kompyuterning miyasi.Inson miyasi singari protsessor ham kompyuterning barcha funksiyalarini boshqaradi.Protsessorning asosiy vazifasi 0 va 1 larni hisoblash.",
+        link: "https://www.youtube.com/watch?v=-TRt_ZDsEi8",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYsdUa5K4pNR3h4ZxXyUZ_3RUki4gQ104Lg&s"
+    },
+]
